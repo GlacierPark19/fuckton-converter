9313374b2f20f3026735d485eb070fde38493c2d
Michael <ThisIsAnEmail@chapman-tech.net>
2024-10-22 00:51:49 -0700

    FInal version

--- fucktons.cpp
+++ fucktons.cpp
@@ -13,8 +13,8 @@ void drawMenu()
     cout << "Please select an option:" << endl;
     cout << "1: Metric to Fucktons" << endl;
     cout << "2: Imperial to Fucktons" << endl;
-    cout << "3: Fucktons to Metric" << endl;
-    cout << "4: Fucktons to Imperial" << endl;
+    // cout << "3: Fucktons to Metric" << endl;
+    // cout << "4: Fucktons to Imperial" << endl;
     cout << "5: Exit" << endl;
     cout << "*************************************************" << endl;
 }
@@ -39,11 +39,11 @@ void menuSelect()
         break;
 
     case 3:
-        ftm();
+
         break;
 
     case 4:
-        fti();
+
         break;
 
     case 5:
@@ -66,23 +66,107 @@ void menuSelect()
 int mtf()
 {
 
-    string inString;
+    string inString, unit;
     double metricWeight, tempWeight, fuckTonWeight, buttLoadMeasurment;
+    int expon;
 
     cout << "Please input starting weight (in Kilos)" << endl;
     getline(cin, inString);
     metricWeight = stod(inString);
     buttLoadMeasurment = (metricWeight / BUTTTON_IN_KILOS);
     cout << "Enter unit of desired measurment: " << endl;
+    struct finalUnit final = buttTonMenu();
+    expon = final.value;
+    unit = final.unit;
+    fuckTonWeight = pow(buttLoadMeasurment, expon);
+
+    cout << "The final weight is: " << fuckTonWeight << " " << unit << endl;
+    return 0;
 }
-itf()
+
+int itf()
 {
+    string inString, unit;
+    double imperialWeight, metricWeight, tempWeight, fuckTonWeight, buttLoadMeasurment;
+    int expon;
+
+    cout << "Please input starting weight (in Pounds)" << endl;
+    getline(cin, inString);
+    imperialWeight = stod(inString);
+    metricWeight = (imperialWeight / 2.205);
+    buttLoadMeasurment = (metricWeight / BUTTTON_IN_KILOS);
+    cout << "Enter unit of desired measurment: " << endl;
+    struct finalUnit final = buttTonMenu();
+    expon = final.value;
+    unit = final.unit;
+    fuckTonWeight = pow(buttLoadMeasurment, expon);
+
+    cout << "The final weight is: " << fuckTonWeight << " " << unit << endl;
+    return 0;
 }
 
-int fti() {}
-
-int ftm() {}
-
-void buttTonMenu()
+struct finalUnit buttTonMenu()
 {
+    struct finalUnit final;
+    string temp;
+    double tempInt;
+    int switchInt;
+    cout << "1: Butt Tons" << endl;
+    cout << "2: Assloads" << endl;
+    cout << "3: Asstons" << endl;
+    cout << "4: Shitloads" << endl;
+    cout << "5: Shittons" << endl;
+    cout << "6: fuckloads" << endl;
+    cout << "7: Fucktons" << endl;
+
+    getline(cin, temp);
+    tempInt = stod(temp);
+    switchInt = stoi(temp);
+
+    switch (switchInt)
+    {
+    case 1:
+
+        final.unit = "Butt Tons";
+        final.value = tempInt;
+        return final;
+        break;
+    case 2:
+        final.unit = "Assloads";
+        final.value = tempInt;
+        return final;
+        break;
+
+    case 3:
+        final.unit = "Asstons";
+        final.value = tempInt;
+        return final;
+        break;
+
+    case 4:
+        final.unit = "Shitload";
+        final.value = tempInt;
+        return final;
+        break;
+
+    case 5:
+        final.unit = "Shitton";
+        final.value = tempInt;
+        return final;
+        break;
+
+    case 6:
+        final.unit = "Fuckload";
+        final.value = tempInt;
+        return final;
+        break;
+
+    case 7:
+        final.unit = "Fuckton";
+        final.value = tempInt;
+        return final;
+        break;
+    default:
+        break;
+    }
 }
--- fucktons.hpp
+++ fucktons.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <cmath>
 #include <string>
 using namespace std;
 const double BUTTTON_IN_KILOS = 1763;
@@ -12,5 +13,11 @@ int fti();
 int ftm();
 void runProgram();
 void drawMenu();
-
+struct finalUnit buttTonMenu();
 void menuSelect();
+
+struct finalUnit
+{
+    string unit;
+    double value;
+};
